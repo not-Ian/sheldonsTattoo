cf47a1609472aeb34242078b0bf43a14fd4e3339
Ian <ianpatrickcunningham@gmail.com>
2020-08-10 18:50:29 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+This is a script to comment on Sheldon's post about getting a tattoo. Post can be found here: https://www.facebook.com/plankton77/posts/10223870342847047
+
+I'm trying to make this easy enough for a not-so-technical person to use.
+
+## Installation
+
+1. Install Python 3. I'm using 3.6.11. Our versions don't have to be exactly the same. Download python here: https://www.python.org/downloads/
+2. Install all the packages this project uses. This can be done by opening up a `terminal` or `command prompt` *(depending on if you're on OSX or Windows)*, navigating to the directory this project is saved in using terminal *(this is done with the `cd` command in terminal. I might run something like this command to go to my project directory on Windows: `cd ".\Development\python\Sheldons Tattoo"` or or something like this command if I'm on OSX: `cd "~/Development/python/Sheldons\ Tattoo`)*. Then finally, run this command: `pip install -r requirements.txt`. This should take some time to install everything, and there might be some prompts asking if you're sure you want to download.
+
+3. You'll also need to install a webdriver in order for one of the Python packages "Selenium" to interact with a web browser. Instructions for that can be found here: https://selenium-python.readthedocs.io/installation.html
+
+I downloaded a Google Chrome driver, found here: https://sites.google.com/a/chromium.org/chromedriver/getting-started
+
+---
+
+Almost there. Now that you've downloaded a driver, you'll need to make a file "credentials.json" in the same directory as this project. This file is used to 1. Log into Facebook with your account and make comments. And 2. store the location you just saved that webdriver to. It will include your Facebook email, Facebook password, and webdriver location. It should look something like this:
+
+```
+{
+    "email": "email@email.com",
+    "password": "Password!",
+    "chromeDriverLocation": "~/location/to/chromedriver/chromedriver.exe"
+}
+```
+
+## Using the script
+
+There's three ways to post a commment. You can select a certain method by uncommenting one line from lines 16, 18, or 21. Leave the other two commented out.
+
+A description of each:
+
+1. `paste` - Post comments from whatever you have copied on your clipboard. This method is the fastest.
+2. `type` - Posts comments from the text on line 20. This is slower since it types it out manually, rather than copy/pasting like `paste` does. 
+3. `script` - Putting various text in a separate file, and having the code read that file line by line, and posting each line separately as a comment. If you want to use your own, save a script as a `.txt` file in this directory, and change line 32 to point to your new script.
+
+There's a variable on line 15 `numComments` - put the number of comments you want to make here if you're using comment type `paste` or `type`.
+
+You can now run the script by opening up your command prompt or terminal and running `python3 ./comment.py`, or running it in your favorite IDE.
+
+#### Misc
+
+It's kind of annoying to stop the script in the middle of running. It'll be trying to type stuff, so you need to go really quickly and `ctrl+c` or whatever command it is to stop.
+You can't really do other things with your computer while this is running. Close your eyes and think about Sheldon at a tattoo shop.
+The slower methods likely have a lower chance of being caught by Facebook's spam filter? I think the script method probably has the lowest since it 1. paces itself, and 2. has different content for each comment.
---
+++ comment.py
@@ -0,0 +1,112 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from pyautogui import press, typewrite, hotkey
+from bs4 import BeautifulSoup
+import json
+import time
+import random
+
+def enum(*args):
+    enums = dict(zip(args, range(len(args))))
+    return type("Enum", (), enums)
+CommentType = enum("paste", "type", "script")
+
+
+numComments = 500 # Only used for paste/type. You probably shouldn't do something like 100 thousand
+# commentMode = CommentType.paste # paste text from clipboard repeatedly (fastest)
+
+# commentMode = CommentType.type # type out something specific over and over (slower, might have a lower chance of being spotted by FB's spam filter?)
+commentToType = "Type whatever you want to be commented here"
+
+commentMode = CommentType.script # read from external file and post each comment separately (slowest, funniest)
+
+with open("./credentials.json", 'r') as file:
+    credentials = json.load(file)
+
+# Do something fun. Save a script (or a book or anything) as a '.txt' file, then load it here.
+# Separate lines that should be comments by new lines.
+# See theChineseRestaurant.txt 
+scriptLines = []
+scriptIndex = 0
+with open("./theChineseRestaurant.txt", 'r') as file:
+    lines = file.readlines() 
+    for line in lines:
+        if not line.isspace():
+            scriptLines.append(line)
+            
+
+emailCredentials = credentials["email"]
+passwordCredentials = credentials["password"]
+chromeDriverLocation = credentials["chromeDriverLocation"]
+
+driver = webdriver.Chrome(chromeDriverLocation)
+driver.get("https://www.facebook.com/plankton77/posts/10223870342847047")
+
+email = driver.find_element_by_id("email")
+password = driver.find_element_by_id("pass")
+
+email.send_keys(emailCredentials)
+password.send_keys(passwordCredentials)
+password.send_keys(Keys.RETURN)
+
+time.sleep(10)
+
+def postComment():
+    global scriptIndex
+    # https://stackoverflow.com/questions/13564851/how-to-generate-keyboard-events-in-python
+    # https://github.com/asweigart/pyautogui
+
+    if commentMode == CommentType.paste:
+        # If you copy whatever you want to post onto your clipboard, the script can paste it instead of typing it manually each time
+        hotkey('ctrl', 'v')
+    elif commentMode == CommentType.type:
+        # Whatever you want to spam on Sheldon's post. Much faster if you just copy it and let the script paste for you.
+        typewrite(commentToType, interval=0.1)
+    elif commentMode == CommentType.script:
+        text = scriptLines[scriptIndex]
+        typewrite(text, interval=0.1)
+        scriptIndex = scriptIndex + 1
+
+    # time.sleep(...) is just a pause - I'm thinking it'll help us avoid Facebook's antispam filter a little bit?
+    time.sleep(0.5)
+    hotkey('\n')
+
+def findCommentField():
+    # TODO: See if alert was thrown that you've hit a comment spam filter? Check if certain wording appears in driver.page_source?
+    # 1. Tap "Okay"?
+    # 2. time.sleep(1 hour)?
+    # 3. Continue the process?
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    divs = soup.find_all('div')
+
+    # I know this is kind of hacky, but something about Facebook changing HTML div class names
+    # after you post a comment makes it difficult to find the comment box otherwise, I think.
+    for div in divs:
+        if div.get('contenteditable'):
+            divClass = div.get('class')[-1]
+
+            commentField = driver.find_element_by_xpath("//div[contains(@class, '" + divClass + "')]")
+            time.sleep(1)
+                    
+            try:
+                # I haven't gotten it to work where it actually types using Selenium's built-in `send_keys("...")`` method. 
+                # `send_keys("...")` might work after you post the first comment?? Not sure. Facebook's HTML is weird.
+                # So, we're using a different package for typing comments: pyautogui
+                commentField.send_keys(" ")
+                postComment()
+                break
+            except:
+                # The first time you find the text field the page reloads so you can't type into it? 
+                # It throws an error, but the cursor is still in the textbox, and we can type using a different package.
+                postComment()
+                break
+
+if commentMode == 3:
+    while scriptIndex < len(scriptLines):
+        findCommentField()
+else:
+    for _ in range(numComments):
+        findCommentField()
+    
+driver.close()
